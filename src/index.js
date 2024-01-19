01d9b2fe91cbe6aa0b6fb68166f60c7b9d5924e7
Georgi Stefanov Todorov <gorqka91@gmail.com>
2024-01-19 14:58:44 +0200

    add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const handlebars = require('express-handlebars');
+const path = require('path')
 
-app.get('/', (req,res) => {
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+app.get('/', (req, res) => {
     res.send('Home Page');
 })
 

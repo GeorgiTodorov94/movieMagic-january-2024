8d68c45fdb8edc733c4cbe798980d47c3e6d298b
Georgi Stefanov Todorov <gorqka91@gmail.com>
2024-02-01 14:37:25 +0200

    Routing Complete

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
+});
+
+router.get('/about', (req,res) => {
+    res.render('about')
+});
+
+router.get('*', (req,res) => {
+    res.render('404')
 });
 
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const router = require('express').Router();
 
 
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController');
+
+
+router.use(movieController);
 router.use(homeController);
 
 module.exports = router;

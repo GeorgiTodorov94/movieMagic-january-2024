d7da38e437620f014ba2cab81de5720697b23b43
Georgi Stefanov Todorov <gorqka91@gmail.com>
2024-02-05 19:22:06 +0200

    add hardCoded Movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,13 @@
-const movies = []
+const movies = [{
+title: 'Jungle Cruise',
+genre: 'Adventure',
+director: 'Pesho',
+date: '1994',
+imageURL: '',
+rating: '10',
+description: 'Description'}]
 
 exports.create = (movieData) => {
+    console.log(movieData)
     movies.push(movieData);
 };

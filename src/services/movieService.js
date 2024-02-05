527b50a96ab9d4f5f57425e56ab9605a7fd721bd
Georgi Stefanov Todorov <gorqka91@gmail.com>
2024-02-05 19:47:27 +0200

    add movie _id's and details link

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,18 @@
-const movies = [];
+const movies = [{
+_id: '1',
+title: 'Jungle Cruise',
+genre: 'Adventure',
+director: 'Pesho',
+date: '1994',
+imageURL: '',
+rating: '10',
+description: 'Description'}];
 
 
 exports.getAll = () => {
     return movies.slice();
 }
 exports.create = (movieData) => {
-    console.log(movieData)
+    movieData._id = movies[movies.length - 1]._id + 1;
     movies.push(movieData);
 };
